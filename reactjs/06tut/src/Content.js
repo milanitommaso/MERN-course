eb65fe6a62177ba3199e07263aff293dc6f27abb
milani <milani.tommaso2004@gmail.com>
2024-07-23 11:02:01 +0200

    chapter 6

---
+++ reactjs/06tut/src/Content.js
@@ -0,0 +1,34 @@
+import {useState} from 'react';
+
+const Content  = () => {
+    const [name, setName] = useState('Tom');
+    const [count, setCount] = useState(0);
+
+    const handleNameChange = () => {
+        const names = ["bob", "kevin", "dave"];
+        const int = Math.floor(Math.random() * 3);
+        setName(names[int]);
+    }
+    
+    const handleClick = () => {
+        setCount(count + 1)
+        setCount(count + 1)
+        console.log(count);
+    }
+    const handleClick2 = (name) => {
+        console.log(count);
+    }
+
+    return (
+        <main>
+            <p onDoubleClick={handleClick}>
+                Hello {name}!
+            </p>
+            <button onClick={handleNameChange}>Change name</button>
+            <button onClick={handleClick}>Click it</button>
+            <button onClick={handleClick2}>Click it</button>
+        </main>
+    )
+}
+
+export default Content
