d2ecffd7bb49119d6d350982067db4463c2a53b3
milani <milani.tommaso2004@gmail.com>
2024-07-23 10:31:07 +0200

    chapter 4

---
+++ reactjs/04tut/src/Content.js
@@ -0,0 +1,19 @@
+
+
+const Content  = () => {
+    const handleNameChange = () => {
+        const names = ["bob", "kevin", "dave"];
+        const int = Math.floor(Math.random() * 3);
+        return names[int];
+    }
+    
+    return (
+        <main>
+            <p>
+                Hello {handleNameChange()}!
+            </p>
+        </main>
+    )
+}
+
+export default Content
